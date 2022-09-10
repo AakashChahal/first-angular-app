0924faeb568da2f8fbbf8f7cc0b7890a9ad642a9
Aakash Chahal <aakash.chahal62@gmail.com>
2022-09-10 20:42:07 +0100

    passing and using data with event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
     allowNewServer = false;
     numClicks = 0;
-    message = this.numClicks
-        ? `button clicked ${this.numClicks} times`
-        : 'button not clicked yet';
+    message = 'button not clicked yet';
+    serverName = '';
 
     constructor() {
         setTimeout(() => (this.allowNewServer = true), 2000);
@@ -20,8 +19,10 @@ export class ServersComponent implements OnInit {
 
     buttonClicked() {
         this.numClicks += 1;
-        this.message = this.numClicks
-            ? `button clicked ${this.numClicks} times`
-            : 'button not clicked yet';
+        this.message = `button clicked ${this.numClicks} times`;
+    }
+
+    onServerNameUpdate(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
     }
 }
