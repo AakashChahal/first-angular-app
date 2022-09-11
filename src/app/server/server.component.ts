bb113e6df5c8a7db3dcb4ec77522dff2f45b7645
Aakash Chahal <aakash.chahal62@gmail.com>
2022-09-11 18:59:39 +0100

    styling elements with [ngStyle] property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 121;
     serverStatus = 'Online';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
+    }
 }
